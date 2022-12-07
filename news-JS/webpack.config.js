a36a30642b7280f1638b6b6007a73af4f6c8c2e5
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2022-12-07 23:02:56 +0300

    feat: add programm settings

--- news-JS/webpack.config.js
+++ news-JS/webpack.config.js
@@ -2,20 +2,21 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index'),
     mode: 'development',
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                test: [/\.css$/i, /\.ts$/i],
+                use: ['style-loader', 'css-loader', 'ts-loader'],
             },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'index.js',
@@ -27,6 +28,9 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
+        new EslingPlugin({ 
+            extensions: 'ts',
+        }),
     ],
 };
 
