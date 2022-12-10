a59e32965fa0a4658a1b2aad331b2e1242a9f226
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2022-12-11 00:14:02 +0300

    feat: add interfaces

---
+++ news-JS/src/types/index.ts
@@ -0,0 +1,49 @@
+// --News data--
+
+export interface INews {
+  status: string;
+  totalResults: number;
+  articles: NewsData;
+}
+
+export interface NewsData {
+  author: string;
+  content: string;
+  description: string;
+  publishedAt: string;
+  source: NewsDataSource;
+  title: string;
+  url: string;
+  urlToImage: string;
+}
+
+export interface NewsDataSource {
+  id: string;
+  name: string;
+}
+
+//  --Source data--
+
+export interface ISources {
+  status: string;
+  sources: SourcesData;
+}
+
+export interface SourcesData {
+  category: string;
+  country: string;
+  description: string;
+  id: string;
+  language: string;
+  name: string;
+  url: string;
+}
+
+//  --Error Status--
+
+export enum ErrorStatus {
+  Unauthorized = 401,
+  PaymentRequired,
+  Forbidden,
+  NotFound,
+}
