fdbda76b59b1b759ed6964fa1e10ee87ae8faadd
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2022-12-11 01:32:46 +0300

    refactor: add appLoader file for ts

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -1,11 +1,9 @@
-import Loader from './loader';
+import { Loader } from './loader';
 
-class AppLoader extends Loader {
+export class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
             apiKey: 'a50fc7f66d704db0b325ff91c449866e', // получите свой ключ https://newsapi.org/
         });
     }
 }
-
-export default AppLoader;
